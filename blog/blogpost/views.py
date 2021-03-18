14ff53468369484af7296fca1a926d1502e6af4c
vaagengule@hotmail.com <vaagengule@hotmail.com>
2021-03-18 23:38:31 +0100

    Legg til templates

--- blog/blogpost/views.py
+++ blog/blogpost/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Blogpost
+
+
+class BlogpostListView(ListView):
+    model = Blogpost
+    template_name = "blogposts_list.html"
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from blog.blogpost.views import BlogpostListView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("blogposts/", BlogpostListView.as_view()),
 ]
